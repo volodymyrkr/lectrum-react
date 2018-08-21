05b7f0b5eca97ae6c5aebee618ad37110cdebdc3
Volodymyr Kravchenko <volodymyr.kravchenko.2000@gmail.com>
2018-08-21 16:06:12 +0300

    Demo test react component with snapshot

---
+++ source/components/Counter/Counter.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './';
+
+import dom from 'react-test-renderer'
+
+const  renderTree = dom.create(
+    <Counter count = {4}/>
+).toJSON();
+
+test('Counter component should correspond to its snapshot', ()=> {
+    expect(renderTree).toMatchSnapshot()
+})
